b2a8ce7d389d7a29a73ad006c91dac8c7ddcda14
Asa-kawa <kh7672ak@gmail.com>
2024-08-31 14:49:42 +0900

    変更

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const all_websites = [
     {"title": "Password", "url": "https://asa-kawa.github.io/Asa-kawaPassword/"},
     {"title": "next-js", "url": "https://asa-kawa.github.io/asa-kawa-next-js/"},
     {"title": "text-reader-next-js", "url": "https://asa-kawa.github.io/asa-kawa-text-reader-next-js/"},
-    {"title": "scratch", "url": "https://scratch.mit.edu/users/rinku10/"}   
-]
+    {"title": "scratch", "url": "https://scratch.mit.edu/users/rinku10/"},
+];
 
 initWebsiteList();
 
